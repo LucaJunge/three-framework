aa28ebfdc454fca072ce183d64b5aba443e14572
Luca Junge <LucaJunge97@gmail.com>
2023-12-27 15:34:53 +0100

    exporting THREE

--- src/Entry.js
+++ src/Entry.js
@@ -1,5 +1,8 @@
 // Main entry point of the library. Import the other files here
 
+// THREE
+import * as THREE from "three"
+
 // Core
 import { App } from "./Core/App"
 import { WebXRHandler } from "./Core/WebXR"
@@ -7,4 +10,4 @@ import { WebXRHandler } from "./Core/WebXR"
 // Utils
 import { createCube, createSphere } from "./Utils/PrimitiveMeshes"
 
-export { App, WebXRHandler, createCube, createSphere }
+export { App, WebXRHandler, createCube, createSphere, THREE }
